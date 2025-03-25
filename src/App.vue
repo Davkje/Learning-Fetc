fcf615f9b1b29e746e76de60229cf7f44f359cd3
David Kjellstrand <david.kjellstrand@gmail.com>
2025-03-25 16:46:37 +0100

    feat: import todos using fetch

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+
+fetch('http://localhost:3000/todos')
+  .then((response) => {
+    console.log(response)
+    return response.json()
+  })
+  .then((data) => {
+    const todoElement = document.getElementById('todoContainer')
+
+    todoElement.innerHTML = data
+      .map(
+        (todo) => `
+      <div class="todo">
+        <span>${todo.content}</span>
+        <span class="done">${todo.done}</span>
+        <span class="date">${todo.date}</span>  
+      </div> 
+    `,
+      )
+      .join('')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 </script>
 
 <template>
@@ -13,7 +37,19 @@ import { RouterLink, RouterView } from 'vue-router'
     </div>
   </header>
 
+  <!-- TODO SKA INTE VA HÄR, FLYTTA TILL BÄTTRE PLATS -->
+  <div id="todoContainer" class="todo-container"></div>
   <RouterView />
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.todo-container {
+  background-color: rgb(209, 209, 209);
+  margin: 1rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  gap: 1rem;
+  display: flex;
+  flex-direction: column;
+}
+</style>
